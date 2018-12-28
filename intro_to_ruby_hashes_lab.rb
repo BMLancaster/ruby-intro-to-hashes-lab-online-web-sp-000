e8488147d74646684a191b56c6c92562b4326b8b
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-12-28 20:29:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 :pieces => 4,
 :names =>{
   :reading_railroad => {},
-  :pennsylvia_railroad => {},
+  :pennsylvania_railroad => {},
   :b_and_o_railroad => {},
   :shortline => {}
   
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 :pieces => 4,
 :names =>{
   :reading_railroad => {"mortgage_value"=>"$100"},
-  :pennsylvia_railroad => {"mortgage_value"=>"$200"},
+  :pennsylvania_railroad => {"mortgage_value"=>"$200"},
   :b_and_o_railroad => {"mortgage_value"=>"$400"},
   :shortline => {"mortgage_value"=>"$800"}
   
